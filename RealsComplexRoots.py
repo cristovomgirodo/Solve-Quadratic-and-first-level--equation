81a724b67eea41c63f1d474fb070517442a55cae
cristovomgirodo <92377043+cristovomgirodo@users.noreply.github.com>
2021-10-16 17:42:23 -0300

    Add files via upload
    The file RunRealsComplexRoots.py will run the RealsComplexRoots.py module. Welcome the all of the GitHub in using the file RunRealsComplexRoots.py to get the roots of the quadratic equation or an only root the first level.

---
+++ RealsComplexRoots.py
@@ -0,0 +1,125 @@
+#
+# The RealsComplexRoots.py module
+# Developed by Cristovom A. Girodo
+# 
+
+import math
+import cmath
+
+def reals_complex_roots():
+    
+    # global variables
+    a, b, c, delta, expression, raisedelta, root, root1, root2 = 0, 0, 0, 0, 0, 0, 0, 0, 0
+    
+    def introduce():
+        coef = eval(input("\t-_- What is the new value? "))
+        return coef
+    
+    def enterData():
+        for j in range(1, 4):
+            if j == 1:
+                print("\n\t* Provide the %dº" %j,"[coefficient].")
+                u = introduce()
+            elif j == 2:
+                print("\n\t* Enter with the %dº" %j,"[coefficient].")
+                v = introduce()
+            elif j == 3:
+                print("\n\t* Give the %dº" %j,"[coefficient].")
+                w = introduce()
+                return u, v, w    # local variables
+          
+    a, b, c = enterData()
+    
+    def answer():
+        print("\n   + The [roots] are: ","\n   + The first  root(root1)=",format(root1,"<10.2f"),"\n   + The second root(root2)=",format(root2,"<10.2f"))
+        return
+    
+    def display_raisedelta():
+        print("   + The value of the [raisedelta] = ",format(raisedelta,"<10.2f"))
+        return
+    
+    def first_root(h, i, j):
+        firt = (-i+j)/(2*h)
+        return firt
+    
+    def second_root(h, i, j):
+        secrt = (-i-j)/(2*h)
+        return secrt
+
+    print("\n\n\t\t============================================")
+    print("\t\t| . . . *[ ANSWER TO THE EQUATION ]* . . . |")
+    print("\t\t============================================\n")
+
+    if a != 0 and b != 0 and c != 0:
+        delta = b**2-4*a*c
+        print("\n   + The new value of [delta] = ", delta)
+        if delta > 0:
+            raisedelta = math.sqrt(delta)
+            view = display_raisedelta()
+            p, q, r = a, b, raisedelta
+            root1 = first_root(p, q, r)
+            root2 = second_root(p, q, r)
+            solution = answer()
+            
+        elif delta < 0:
+            print("\n   --------------------------------------------------------------------")
+            print("   * When the value of delta is negative the equation: a*x**2+b*x+c = 0\n\
+            to theorem have not reals roots. *")
+            print("   --------------------------------------------------------------------")
+            print("\n\n\t   * Will get only two complexs roots *\n")
+            raisedelta = cmath.sqrt(delta)
+            view = display_raisedelta()
+            p, q, r = a, b, raisedelta
+            root1 = first_root(p, q, r)
+            root2 = second_root(p, q, r)
+            solution = answer()
+            
+        elif delta == 0:
+            root1 = root2 = (-b+0.0)/(2*a)
+            solution = answer()
+            
+    elif a > 0 and b == 0 and c > 0 or a < 0 and b == 0 and c < 0:
+        print("\n  - The new equation of the second level geted will: [ a*x**2+c = 0 or\n\
+    -a*x**2-c = 0 ] and will have two complexs roots.\n")
+        expression = -((c + 0.0)/a)
+        root1 = cmath.sqrt(expression)
+        root2 = -cmath.sqrt(expression)
+        print("\n   + The value of the expression:[ -c/a ] will:",format(expression,"<10.2f"))
+        solution = answer()
+        
+    elif a < 0 and b == 0 and c > 0 or a > 0 and b == 0 and c < 0:
+        print("\n  - The new equation of the second level geted will: [ -a*x**2+c = 0 or\n\
+    a*x**2-c = 0 ] and will have two reals roots.\n")
+        expression = -(c + 0.0)/a
+        root1 = math.sqrt(expression)
+        root2 = -math.sqrt(expression)
+        print("\n   + The value of the expression: [ c/a ] will: ",format(expression,"<10.2f"))
+        solution = answer()
+        
+    elif a > 0 and b > 0 and c == 0 or a < 0 and b < 0 and c == 0 or a < 0 and b > 0 and c == 0 or a > 0 and b < 0 and c == 0:
+        print("\n  - The new equation of the second level geted will an of the followings:\n\
+    [ a*x**2+b*x = 0 or -a*x**2-b*x = 0 or a*x**2-b*x = 0 or -a*x**2+b*x = 0 ]\n\
+      and will have two reals roots.\n")
+        root1 = 0
+        root2 = -(b+0.0)/a
+        solution = answer()
+        
+    elif a == 0 and b != 0 and c != 0:
+        print("\n  - The new equation of the first level geted will: [ b*x+c = 0 ]\n    and will have an only real root.")
+        if b > 0 and c > 0 or b < 0 and c < 0:
+            root = (-c + 0.0) / b
+        elif b > 0 and c < 0 or b < 0 and c > 0:
+            root = (c + 0.0) / b
+        
+        print("\n\n\t+ The new real root will: ",format(root,"<10.2f"))
+
+    print("\n\n  =====================================================================")
+    print("  |º>º If necessary process the RealsComplexRoots.py module again! º>º|")
+    print("  =====================================================================")
+
+
+reals_complex_roots()
+
+
+
+
---
+++ RunRealsComplexRoots.py
@@ -0,0 +1,19 @@
+#==================================================================================================
+#=  The RealsComplexRoots.py module will find the roots: root1 and root2 ( reals or complexs ) 
+#=  Date: 20200515 New Version: 4.0                                                                               
+#=  Developed by Cristovom A.Girodo                                                                
+#==================================================================================================
+
+print("\n\n\n\t========================================================")
+print("\t[   Welcome using the The RealsComplexRoots.py module  ]")
+print("\t--------------------------------------------------------")
+print("\t -_- Find the roots: root1 and root2 reals or complexs ")
+print("\t========================================================")
+print("\t\t _-_ New Version: 4.0 -- [ Stable ] _-_")
+print("\t\t    [Developed by Cristovom A.Girodo]\n\n")
+
+from RealsComplexRoots import reals_complex_roots
+
+input("\n\n\t\t . . .Key [ENTER] to exit, Ok!. . .")
+
+
