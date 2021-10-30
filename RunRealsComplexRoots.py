6f714ab1ccec13cf3aa231a98e728b1681b54501
cristovomgirodo <92377043+cristovomgirodo@users.noreply.github.com>
2021-10-30 16:32:07 -0300

    Update RunRealsComplexRoots.py
    The file The RealsComplexRoots.py module was update Version: 4.3( Stable )

--- RunRealsComplexRoots.py
+++ RunRealsComplexRoots.py
@@ -1,6 +1,6 @@
 #==================================================================================================
 #=  The RealsComplexRoots.py module will find the roots: root1 and root2 ( reals or complexs ) 
-#=  Date: 20211021 New Version: 4.2                                                                             
+#=  Date: 20211030 New Version: 4.3(Stable)                                                                             
 #=  Developed by Cristovom A.Girodo                                                                
 #==================================================================================================
 
@@ -9,7 +9,7 @@ print("\t  [   Welcome using The RealsComplexRoots.py module  ]")
 print("\t--------------------------------------------------------")
 print("\t -_- Find the roots: root1 and root2 reals or complexs ")
 print("\t========================================================")
-print("\t\t _-_ New Version: 4.2 -- [ Stable ] _-_")
+print("\t\t _-_ New Version: 4.3 -- [ Stable ] _-_")
 print("\t\t    [Developed by Cristovom A.Girodo]\n\n")
 
 from RealsComplexRoots import reals_complex_roots
